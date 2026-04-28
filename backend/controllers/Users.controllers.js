8c14a0ab00ee9abbe544862b95bb9f3640ea84ed
Apeksha-99 <chaudhariapeksha62@gmail.com>
2026-04-28 16:47:24 +0530

    profile update

--- backend/controllers/Users.controllers.js
+++ backend/controllers/Users.controllers.js
@@ -110,17 +110,12 @@ export const updateProfile = async (req,res) =>{
     try {
         const {fullname, email, phoneNumber, bio, skills} = req.body;
         const file = req.file;
-         if(!fullname || !email || !phoneNumber || !bio || !skills){
-            return res.status(400).json({
-                message:"Something is missing",
-                success: false
-            })
-        };
 
+let skillsArray;
+        if(skills){
+            skillsArray = skills.split(',');
+        }
 
-
-
-        const skillsArray = skills.split(',');
         const userId = req.id;
         let User = await User.findById(userId);
         if(!user){
@@ -129,12 +124,12 @@ export const updateProfile = async (req,res) =>{
                 success: false
             })
         }
-        user.fullname = fullname,
-        user.email = email,
-        user.phoneNumber = phoneNumber,
-        user.profile.bio = bio,
-        user.profile.skills = skillsArray
 
+        if(fullname) user.fullname = fullname
+        if(email) user.email = email
+        if(phoneNumber) user.phoneNumber = phoneNumber
+        if(bio)user.profile.bio = bio
+        if(skills)user.profile.skills = skillsArray
 
 
         await user.save();
--- backend/routes/User.route.js
+++ backend/routes/User.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { login, register, updateProfile } from '../controllers/Users.controllers';
+import { login, logout, register, updateProfile } from '../controllers/Users.controllers';
 import isAuthenticated from '../middlewares/IsAuthenticated';
 
 const router = express.Router();
 
 router.route('/register').post(register)
 router.route('/login').post(login)
+router.route('/logout').get(logout)
 router.route('/profile/update').post(isAuthenticated, updateProfile)
 
 export default router;
